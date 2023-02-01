aeb859b84a60b952fd949ca80dabf1da59fc2c78
MR-Addict <2750417853@qq.com>
2023-02-02 02:51:00 +0800

    获取超过100条提交

--- src/app/(about)/commits/fetchCommits.ts
+++ src/app/(about)/commits/fetchCommits.ts
@@ -1,12 +1,43 @@
 import { formatDate, groupBy } from "@/lib/utils";
 
-async function fetchdata() {
-  const repository = "MR-Addict/playground";
-  const res = await fetch(`https://api.github.com/repos/${repository}/commits?per_page=100`, { cache: "force-cache" });
-  if (!res.ok) throw new Error("Failed to fetch data");
-  const result = await res.json();
+const per_page = 30;
 
-  const shrinkedData: { date: string; message: string }[] = result.map((item: any) => ({
+interface CommitType {
+  date: string;
+  message: string;
+}
+
+function chunk(array: CommitType[], size: number) {
+  var results = [];
+  while (array.length) results.push(array.splice(0, size));
+  return results;
+}
+
+async function fetchData() {
+  let page = 1;
+  const data: any[] = [];
+
+  async function fetchOnePage() {
+    const res = await fetch(
+      `https://api.github.com/repos/MR-Addict/playground/commits?per_page=${per_page}&page=${page}`,
+      { headers: { Authorization: "Basic " + Buffer.from(`MR-Addict:${process.env.GITHUB_TOKEN}`).toString("base64") } }
+    );
+    if (!res.ok) throw new Error("Failed to fetch data");
+    const result: any[] = await res.json();
+    return result;
+  }
+
+  while (true) {
+    const result = await fetchOnePage();
+    data.push(...result);
+    if (result.length < per_page) {
+      if (result.length === 0) --page;
+      break;
+    }
+    page++;
+  }
+
+  const shrinkedData: CommitType[] = data.map((item: any) => ({
     date: formatDate(item.commit.committer.date),
     message: item.commit.message,
   }));
@@ -14,7 +45,7 @@ async function fetchdata() {
 }
 
 export default async function fetchCommits() {
-  const res = await fetchdata();
-  const result = groupBy(res, (commit) => commit.date.split(" ")[0]);
-  return result;
+  const res = await fetchData();
+
+  return groupBy(res, (commit) => commit.date.split(" ")[0]);
 }

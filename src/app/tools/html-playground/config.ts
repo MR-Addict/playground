a86d71b05ba590b5a600823f20599355d38b75f7
MR-Addict <2750417853@qq.com>
2023-02-04 14:36:57 +0800

    调整默认样式

--- src/app/tools/html-playground/config.ts
+++ src/app/tools/html-playground/config.ts
@@ -1,8 +1,8 @@
 export const defaultIframeStyle =
-  "*{padding:0;margin:0;box-sizing:border-box;}*::-webkit-scrollbar{width:0.4em;height:0.4rem;}*::-webkit-scrollbar-thumb{background-color:gray;border-radius:0.15rem;}*::-webkit-scrollbar-corner{display:none;}";
+  "*::-webkit-scrollbar{width:0.4em;height:0.4rem;}*::-webkit-scrollbar-thumb{background-color:gray;border-radius:0.15rem;}*::-webkit-scrollbar-corner{display:none;}";
 
 export const defaultInput = {
   html: "<h1>Hello world!</h1>",
-  css: "body{\n  color:white;\n}",
+  css: "*{\n  margin:0;\n  padding:0;\n  box-sizing:border-box;\n}\n\nh1{\n  color:white;\n}",
   js: "document.body.style.background='black';",
 };

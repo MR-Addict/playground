e4a56148abe8df896825436b5f2d5c5278c45646
MR-Addict <2750417853@qq.com>
2023-03-26 22:32:49 +0800

    添加gpt设置选项

--- src/app/(server)/chat/components/MobileUser/MobileUser.tsx
+++ src/app/(server)/chat/components/MobileUser/MobileUser.tsx
@@ -1,21 +1,43 @@
+"use client";
+
+import { useState } from "react";
 import { AiOutlineClear } from "react-icons/ai";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 
 import style from "./MobileUser.module.css";
+import Settings from "../Settings/Settings";
 import { useChatContext } from "../Chat/ChatProvider";
 
 export default function MobileUser() {
   const { resetMessages, chatgptStatus } = useChatContext();
+  const [openSettings, setOpenSettings] = useState(false);
 
   return (
     <div aria-label='user setting' className={style.user}>
-      <button onClick={resetMessages} disabled={chatgptStatus === "thinking"} className={[style.btn].join(" ")}>
+      <button
+        onClick={resetMessages}
+        aria-label='clear history'
+        disabled={chatgptStatus === "thinking"}
+        className={[style.btn].join(" ")}
+      >
         <AiOutlineClear size={20} />
       </button>
 
-      <button type='button' className={[style.hamburger].join(" ")} aria-label='mobile nav button to toggle menu'>
+      <button
+        onClick={() => setOpenSettings(!openSettings)}
+        type='button'
+        className={[style.hamburger, openSettings ? style.active : ""].join(" ")}
+        aria-label='mobile nav button to toggle menu'
+      >
         <div></div>
         <div></div>
       </button>
+
+      {openSettings && (
+        <div className='absolute left-0 w-full -bottom-[27rem] z-10 text-white'>
+          <Settings />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/app/(server)/chat/components/Settings/Settings.tsx
@@ -0,0 +1,104 @@
+import style from "./Settings.module.css";
+
+import { AiOutlineClear } from "react-icons/ai";
+import { useChatContext } from "../Chat/ChatProvider";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+
+export default function Settings() {
+  const { messages, setMessages, generateResponse, options, setOptions, resetMessages, chatgptStatus } =
+    useChatContext();
+
+  return (
+    <div className='w-full h-full flex flex-col gap-4 bg-slate-700 px-3 py-4 rounded-b-md md:rounded-b-none md:p-0'>
+      <div className='flex flex-col gap-3'>
+        <button
+          aria-label='regenerate button'
+          type='button'
+          onClick={() => {
+            const slicedMessages = messages.slice(0, -1);
+            setMessages(slicedMessages);
+            generateResponse(slicedMessages);
+          }}
+          className={[style.btn, "border border-gray-500"].join(" ")}
+        >
+          <IoChatbubbleEllipsesOutline size={18} />
+          <span>Regnerate</span>
+        </button>
+
+        <button
+          onClick={resetMessages}
+          disabled={chatgptStatus === "thinking"}
+          className={[style.btn, "border border-gray-500"].join(" ")}
+        >
+          <AiOutlineClear size={20} />
+          <span>Clear histroy</span>
+        </button>
+      </div>
+
+      <div className='flex flex-col gpa-0.5'>
+        <h1>Settings</h1>
+
+        <div className='p-2 border border-gray-500 rounded-md flex flex-col gap-2'>
+          <div className={style.inputelement}>
+            <label htmlFor='model'>Model</label>
+            <select
+              required
+              id='model'
+              name='model'
+              value={options.model}
+              onChange={(e) => setOptions({ ...options, [e.target.name]: e.target.value })}
+              className={[style.input].join(" ")}
+            >
+              <option value='gpt-3.5-turbo'>gpt-3.5-turbo</option>
+            </select>
+          </div>
+
+          <div className={style.inputelement}>
+            <label htmlFor='temperature'>Temperature</label>
+            <input
+              type='number'
+              min={0}
+              max={1}
+              value={options.temperature}
+              id='temperature'
+              name='temperature'
+              onChange={(e) => setOptions({ ...options, [e.target.name]: Number(e.target.value) })}
+              className={style.input}
+              required
+            />
+          </div>
+
+          <div className={style.inputelement}>
+            <label htmlFor='top_p'>Top p</label>
+            <input
+              type='number'
+              min={0}
+              max={1}
+              id='top_p'
+              name='top_p'
+              onChange={(e) => setOptions({ ...options, [e.target.name]: Number(e.target.value) })}
+              value={options.top_p}
+              className={style.input}
+              required
+            />
+          </div>
+
+          <div className={style.inputelement}>
+            <label htmlFor='max_tokens'>Max Tokens</label>
+            <input
+              type='number'
+              min={1}
+              max={2048}
+              value={options.max_tokens}
+              onChange={(e) => setOptions({ ...options, [e.target.name]: Number(e.target.value) })}
+              id='max_tokens'
+              name='max_tokens'
+              className={style.input}
+              required
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

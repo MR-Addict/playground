b597888207cf30e96f8e824d7ebfcbe19052a791
MR-Addict <2750417853@qq.com>
2023-04-06 14:41:22 +0800

    update hover effect

--- src/components/server/Navbar/components/Usericon/Usericon.tsx
+++ src/components/server/Navbar/components/Usericon/Usericon.tsx
@@ -9,27 +9,17 @@ import { AiOutlineUser } from "react-icons/ai";
 import style from "./Usericon.module.css";
 import { ClientLink } from "@/components/client";
 import { LoadingDots } from "@/components/server";
-import { useWindowSize, useClickOutside } from "@/hooks";
 
 export default function Usericon({ session }: { session: Session }) {
-  const windowSize = useWindowSize();
   const dropmenuRef = useRef<HTMLDivElement>(null);
   const [isLoggingOut, setIsLoggingOut] = useState(false);
-  const [isActive, setIsActive] = useState(false);
-
-  useClickOutside(dropmenuRef, () => setIsActive(false));
 
   return (
     <div ref={dropmenuRef} className={style.usericon}>
-      <button
-        aria-label='user icon button'
-        disabled={windowSize.width > 768}
-        onClick={() => setIsActive(!isActive)}
-        type='button'
-      >
+      <button aria-label='user icon button' type='button'>
         <AiOutlineUser size={20} />
       </button>
-      <div className={classNames(style.dropmenu, "background", isActive ? style.active : "")}>
+      <div className={classNames(style.dropmenu, "background")}>
         <div>
           <p>Hello, {session.user.username}.</p>
           <p className='text-sm text-gray-500'>{session.user.email}</p>

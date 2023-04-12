72e1c928f618f7c4e2334cbd4e8ea7ec2bc12049
MR-Addict <2750417853@qq.com>
2023-04-13 01:03:34 +0800

    fix copy multi line issue

--- src/lib/utils/copyToClipboard.ts
+++ src/lib/utils/copyToClipboard.ts
@@ -1,8 +1,10 @@
 export default function copyToClipboard(text: string) {
+  const content = text.replaceAll("\n\n", "\n");
+
   return new Promise((resolve, reject) => {
     if (navigator?.clipboard) {
       const cb = navigator.clipboard;
-      cb.writeText(text).then(resolve).catch(reject);
+      cb.writeText(content).then(resolve).catch(reject);
     } else {
       try {
         const body = document.querySelector("body");
@@ -10,7 +12,7 @@ export default function copyToClipboard(text: string) {
         const textarea = document.createElement("textarea");
         body?.appendChild(textarea);
 
-        textarea.value = text;
+        textarea.value = content;
         textarea.select();
         document.execCommand("copy");
 

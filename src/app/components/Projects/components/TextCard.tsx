033d80b56a22f8fd90d95f4d7657012785c5ded5
Cael <MR-Addict@qq.com>
2023-12-17 18:01:11 +0800

    feat: update home page projects

--- src/app/components/Projects/components/TextCard.tsx
+++ src/app/components/Projects/components/TextCard.tsx
@@ -6,7 +6,7 @@ export default function TextCard({ project, isEven }: { project: projectType; is
   return (
     <div className={`flex flex-col items-start ${isEven ? "md:items-start" : "md:items-end"} justify-center gap-4`}>
       <h1 className="text-gray-800 font-bold text-3xl">{project.title}</h1>
-      <p className="w-full max-w-md text-xl text-gray-500">{project.intro}</p>
+      <p className="w-full max-w-md text-xl text-gray-700">{project.intro}</p>
       <div className="flex flex-row items-center gap-2 text-gray-700">
         <a
           href={project.link}
--- src/app/components/Projects/config.ts
+++ src/app/components/Projects/config.ts
@@ -1,5 +1,6 @@
 import { StaticImageData } from "next/image";
 
+import punch from "./images/punch.jpeg";
 import vistalab from "./images/vistalab.png";
 import linktree from "./images/linktree.png";
 import publicapi from "./images/publicapi.png";
@@ -13,18 +14,25 @@ export interface projectType {
 
 const projects: projectType[] = [
   {
+    title: "值班笔记",
+    img: punch,
+    link: "https://github.com/MR-Addict/punch-next",
+    intro:
+      "南京工业大学校大学生科协技术开发部成员的每日值班笔记，允许任何人查看，每学期的内容都自动归档生成本地json文件托管在GitHub上，另外数据库使用了SSR和ISR技术使得访问极其快速。"
+  },
+  {
+    title: "远景实验室主页",
+    img: vistalab,
+    link: "https://github.com/510Lab/vistalab-frontend",
+    intro:
+      "南京工业大学远景实验室的主页，实验室成员可以注册登录查看一些实验室的信息，包括成员信息，实验室项目，实验室设备等等，还可以在线提交发票方便统一管理。"
+  },
+  {
     title: "Public APIs frontend",
     img: publicapi,
     link: "https://github.com/mr-addict/public-apis-frontend",
     intro:
       "This is my first independent nextjs 13 project rather than learning or copying from Youtube videos or other platforms. I spent a lot of time debugging, especially css style. I should confess that I lack innovation in design. Even though I still like it. I'm sure that you can find free and interesting public apis from this app."
-  },
-  {
-    title: "Vistalab",
-    img: vistalab,
-    link: "https://github.com/510Lab/vistalab-frontend",
-    intro:
-      "A project we built for the organization I joined in my university. We built this website after many years that it almost faded out its past glory. It's my winter vacation in 2022 december approaching 2023 when we built this project. I'm reponsible for frontend, and my friend also laboratory reopen founder is responsible for backend. Hope this time it will glory again."
   },
   {
     title: "Linktree",
